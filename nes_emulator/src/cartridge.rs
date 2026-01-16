cf3dcb549c6df508562697522245eda26a2f359d
juliansalvador727 <juliansalvador727@gmail.com>
2026-01-16 12:38:26 -0500

    four screen and vertical mirroring

--- nes_emulator/src/cartridge.rs
+++ nes_emulator/src/cartridge.rs
@@ -24,5 +24,26 @@ impl Rom {
         if ines_ver != 0 {
             returm Err("NES2.0 not supported".to_string());        
         }
+        let four_screen = raw[6] & 0b1000 != 0;
+        let vertical_mirroring = raw[6] & 0b1 != 0;
+        let screen_mirroring = match (four_screen, vertical_mirroring,) {
+            (true, _) => Mirroring::FourScreen;
+            (false, true) => Mirroring:Vertical;
+            (false, false) => Mirroring::Horizontal;
+        }
+        let prg_rom_size = raw[4] as usize * PRG_ROM_PAGE_SIZE;
+        let chr_rom_size = raw[5] as usize * CHR_ROM_PAGE_SIZE;
+
+        let skip_trainer = raw[6] & 0b100 != 0;
+
+        let prg_rom_start = 16 + if skip_trainger { 512 } else { 0 };
+        let chr_rom_start = prg_rom_start + prg_rom_size;
+
+        Ok(Rom {
+            prg_rom: raw[prg_rom_start..(prg_rom_start + prg_rom_size)].to_vec(),
+            chr_rom: raw[chr_rom_start..(chr_rom_start + chr_rom_size)].to_vec(),
+            mapper: mapper,
+            screen_mirroring: screen_mirroring,
+        })
     }
 }
