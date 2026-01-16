5221d986564bdd7bbf918ff41619f29c54793c04
juliansalvador727 <juliansalvador727@gmail.com>
2026-01-16 12:54:42 -0500

    snake working w/ cartridge

--- nes_emulator/src/cpu.rs
+++ nes_emulator/src/cpu.rs
@@ -876,12 +876,15 @@ impl CPU {
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::cartridge::test;
 
     #[test]
     fn test_0xa9_lda_immediate_load_data() {
-        let bus = Bus::new();
+        let bus = Bus::new(test::test_rom(vec![0xa9, 0x05, 0x00]));
         let mut cpu = CPU::new(bus);
-        cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
+
+        cpu.run();
+
         assert_eq!(cpu.register_a, 5);
         assert!(cpu.status.bits() & 0b0000_0010 == 0b00);
         assert!(cpu.status.bits() & 0b1000_0000 == 0);
@@ -889,40 +892,43 @@ mod test {
 
     #[test]
     fn test_0xaa_tax_move_a_to_x() {
-        let bus = Bus::new();
+        let bus = Bus::new(test::test_rom(vec![0xaa, 0x00]));
         let mut cpu = CPU::new(bus);
         cpu.register_a = 10;
-        cpu.load_and_run(vec![0xaa, 0x00]);
+
+        cpu.run();
 
         assert_eq!(cpu.register_x, 10)
     }
 
     #[test]
     fn test_5_ops_working_together() {
-        let bus = Bus::new();
+        let bus = Bus::new(test::test_rom(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]));
         let mut cpu = CPU::new(bus);
-        cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+
+        cpu.run();
 
         assert_eq!(cpu.register_x, 0xc1)
     }
 
     #[test]
     fn test_inx_overflow() {
-        let bus = Bus::new();
+        let bus = Bus::new(test::test_rom(vec![0xe8, 0xe8, 0x00]));
         let mut cpu = CPU::new(bus);
         cpu.register_x = 0xff;
-        cpu.load_and_run(vec![0xe8, 0xe8, 0x00]);
+
+        cpu.run();
 
         assert_eq!(cpu.register_x, 1)
     }
 
     #[test]
     fn test_lda_from_memory() {
-        let bus = Bus::new();
+        let bus = Bus::new(test::test_rom(vec![0xa5, 0x10, 0x00]));
         let mut cpu = CPU::new(bus);
         cpu.mem_write(0x10, 0x55);
 
-        cpu.load_and_run(vec![0xa5, 0x10, 0x00]);
+        cpu.run();
 
         assert_eq!(cpu.register_a, 0x55);
     }
